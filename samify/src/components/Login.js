a6addf9e905879de5485d8ad424fc05d6784b246
HeyyD <heidi.hati@cs.tamk.fi>
2018-09-08 21:55:51 +0300

    Login layout

--- samify/src/components/Login.js
+++ samify/src/components/Login.js
@@ -4,12 +4,16 @@ import './Login.style.scss';
 class Login extends Component {
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" placeholder="Username"></input>
-          <input type="password" placeholder="Password"></input>
-          <button>Login</button>
-        </form>
+      <div className="login-form">
+        <div className="login-background"></div>
+        <div className="form-container">
+          <form>
+            <h1>Welcome to Samify</h1>
+            <input type="text" placeholder="Username"></input>
+            <input type="password" placeholder="Password"></input>
+            <button>Login</button>
+          </form>
+        </div>
       </div>
     );
   }
