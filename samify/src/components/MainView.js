63ffa13fa9cc1abc4d240a251eff35623c7d5588
HeyyD <heidi.hati@cs.tamk.fi>
2018-09-09 12:10:17 +0300

    Add main view component

---
+++ samify/src/components/MainView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SongList from './SongList';
+
+class MainView extends Component {
+
+  render() {
+    return (
+      <SongList/>
+    );
+  }
+}
+
+export default MainView;
