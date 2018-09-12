50a819096f130515ef84144999395a761230bae3
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-09-12 18:46:43 +0300

    Modify App new route for ArtistPage

--- samify/src/App.js
+++ samify/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Login from './components/Login';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import MainView from './components/MainView';
+import ArtistPage from './components/ArtistPage';
 
 class App extends Component {
   render() {
@@ -9,8 +10,9 @@ class App extends Component {
       <Router>
         <div className="page-container">
           <Switch>
+            <Route exact path='/' component={ MainView } />
             <Route path='/login' component={ Login }/>
-            <Route path='/' component={ MainView } />
+            <Route path='/artist' component={ArtistPage} />
           </Switch>
         </div>
       </Router>
