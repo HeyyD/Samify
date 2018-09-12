da7dc3c12b8afc0796f62e9b137e79bfb622084f
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-09-12 18:45:25 +0300

    Modify ArtistPage add image

--- samify/src/components/ArtistPage.js
+++ samify/src/components/ArtistPage.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const ArtistPage = () => {
-  return( <div>moi</div>);
+  return(
+    <div>
+      <image src='http://www.amiright.com/album-covers/images/album-Rick-Springfield-Working-Class-Dog.jpg' />
+      moi
+    </div>
+  );
 };
 
 export default ArtistPage;
