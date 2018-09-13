1002f49ba0d9e258dd00b50985955224b1583160
Ch4nge <sami.hautamaki@cs.tamk.fi>
2018-09-13 20:08:48 +0300

    Modify ArtistPage ProgressBar

--- samify/src/components/ArtistPage.js
+++ samify/src/components/ArtistPage.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import SongList from './SongList';
+import { Line } from 'rc-progress';
 import './ArtistPage.style.scss';
 
 const ArtistPage = () => {
   return(
-    <div className='artistWrapper'>
-      <img src='http://www.amiright.com/album-covers/images/album-Rick-Springfield-Working-Class-Dog.jpg' />
-      <div className='popular'>
-        <h2>Loki</h2>
-        <SongList />
+    <div>
+      <div className='artistWrapper'>
+        <img src='http://www.amiright.com/album-covers/images/album-Rick-Springfield-Working-Class-Dog.jpg' />
+        <div className='popular'>
+          <h2>Loki</h2>
+          <SongList />
+        </div>
+      </div>
+      <div className='donationBar'>
+        <Line
+          percent="10"
+          strokeWidth="10"
+          trailWidth="10"
+          strokeColor="#2BC252"
+          trailColor="#928f8f" />
       </div>
     </div>
   );
